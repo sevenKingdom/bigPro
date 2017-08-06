62c17e7b5c7a4d19dfc398a4bb71919f4f784b52
jinming <jinming@xiaoshouyi.com>
2017-08-06 18:21:16 +0800

    change something

--- src/main/java/com/carry/model/mysql/dao/ConstructionPlanMapper.java
+++ src/main/java/com/carry/model/mysql/dao/ConstructionPlanMapper.java
@@ -13,27 +13,27 @@ import java.util.List;
 @Mapper
 @Repository
 public interface ConstructionPlanMapper {
-    @Select("select * from construction_plan_t where created_at >= #{startTime} and created_at <= #{endTime} and technician_id in (#{technicianIds})")
+    @Select("select * from construction_plan_t where createdAt >= #{startTime} and createdAt <= #{endTime} and technicianId in (${technicianIds})")
     List<ConstructionPlan> getListByTime(@Param("startTime") Long startTime, @Param("endTime") Long endTime, @Param("technicianIds") String technicianIds);
 
-    @Insert("insert into construction_plan_t(bridge_name,pier_num,structure,process,category,is_need_demonstrate," +
-            "construction_plan_file,is_need_approve,technician_id,created_at) values (#{bridgeName},#{pierNum},#{structure},#{process}" +
+    @Insert("insert into construction_plan_t(bridgeName,pierNum,structure,process,category,isNeedDemonstrate," +
+            "constructionPlanFile,isNeedApprove,technicianId,createdAt) values (#{bridgeName},#{pierNum},#{structure},#{process}" +
             ",#{category},#{isNeedDemonstrate},#{constructionPlanFile},#{isNeedApprove},#{technicianId},#{createdAt})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     long saveConstructionPlan(ConstructionPlan constructionPlan);
 
 
     @Select(" select department from user_data where id in " +
-            "(select technician_id from construction_plan_t where id = #{id} )")
+            "(select technicianId from construction_plan_t where id = #{id} )")
     String getPlanAuthor (@Param("id") Long id) ;
 
-    @Update("update construction_plan_t set inspector_id = #{inspectorid} where id = #{id}")
+    @Update("update construction_plan_t set inspectorId = #{inspectorid} where id = #{id}")
     long updatePlanInspector (@Param("inspectorid") Long inspectorid,@Param("id") Long id) ;
 
     @Select(" select id from user_data where department = #{department} and role = 1")
     List<Long> getTechnicianid(@Param("department") String department);
 
-    @Select("select * from construction_plan_t where inspector_id = #{inspectorid} " +
-            "and created_at >= #{startTime} and created_at <= #{endTime}")
+    @Select("select * from construction_plan_t where inspectorId = #{inspectorid} " +
+            "and createdAt >= #{startTime} and createdAt <= #{endTime}")
     List<ConstructionPlan> getListByInspectorid(@Param("inspectorid") Long inspectorid,@Param("startTime") Long startTime, @Param("endTime") Long endTime);
 }
